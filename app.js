06c20cd244da87dc0b02176680067b42b67e2f51
AlexCano08 <cano_alejandro97@hotmail.com>
2023-06-27 21:09:27 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+
+app.use(express.static(path.resolve(__dirname, "./public")))
+
+
+
+app.listen(PORT, () => console.log("Escuchando en http://localhost:" + PORT))
